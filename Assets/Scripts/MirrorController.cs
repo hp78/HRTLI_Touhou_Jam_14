1bca6cb0e8e585e176a074eaabebecc19602e135
hp78 <hp78rox@gmail.com>
2024-10-26 10:04:05 +0800

    Initial commit

---
+++ Assets/Scripts/MirrorController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class MirrorController : MonoBehaviour
+{
+    public PlayerController pc;
+    public ReflectionController rc;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        rc.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // is in mirror view
+        if(pc.transform.position.y < (transform.position.y + 1) && pc.transform.position.y > (transform.position.y - 1))
+        {
+            rc.gameObject.SetActive(true);
+            rc.transform.position = new Vector3((-pc.transform.position.x + transform.position.x + transform.position.x), pc.transform.position.y);
+        }
+        else
+        {
+            rc.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    public bool isAlive = true;
+    public PlayerInput playerInput;
+    public SpriteRenderer spriteRender;
+    public MirrorController mirrorControl;
+
+    Vector2 _movementInput;
+    float _xInput;
+
+    float _moveForce = 10f;
+    float _jumpForce = 14f;
+    float _jumpThreshold = 1.5f;
+
+    float _playerFacing = 1f;
+
+    bool _isJumpKeyDown = false;
+    bool _inAir = false;
+
+
+    int _layermask;
+
+    Rigidbody2D _rigidbody;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _movementInput = Vector2.zero;
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _layermask = (1 << 8); //Player
+        _layermask |= (1 << 9);//enemy
+        _layermask |= (1 << 10);//camera
+        _layermask = ~_layermask;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+        UpdateJump();
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        _xInput = ctx.ReadValue<Vector2>().x;
+
+        if (_xInput > 0)
+        {
+            spriteRender.flipX = true;
+            _playerFacing = 1;
+        }
+        if (_xInput < 0)
+        {
+            _playerFacing = -1;
+            spriteRender.flipX = false;
+        }
+    }
+
+    void UpdateMovement()
+    {
+        _rigidbody.linearVelocity = new Vector2(_xInput * _moveForce, _rigidbody.linearVelocity.y);
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        _isJumpKeyDown = ctx.performed;
+    }
+    void UpdateJump()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + 0.5f, transform.position.y), 
+            Vector2.down, _jumpThreshold, _layermask);
+        RaycastHit2D hit2 = Physics2D.Raycast(new Vector2(transform.position.x - 0.5f, transform.position.y), 
+            Vector2.down, _jumpThreshold, _layermask);
+        //Gizmos.color = Color.cyan;
+
+        if (hit || hit2)
+        {
+            _inAir = false;
+            //anim.SetTrigger("TriggerLand");
+
+            //Debug.DrawLine(new Vector2(transform.position.x + 0.5f, transform.position.y), hit.point, Color.cyan);
+            //Debug.DrawLine(new Vector2(transform.position.x - 0.5f, transform.position.y), hit2.point, Color.cyan);
+        }
+        else
+        {
+            _inAir = true;
+        }
+
+        if (!_inAir && _isJumpKeyDown)
+        {
+            _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, _jumpForce);
+            //anim.SetTrigger("TriggerJump");
+        }
+    }
+
+    public void OnMirror(InputAction.CallbackContext ctx)
+    {
+        if(ctx.performed)
+        {
+            mirrorControl.gameObject.SetActive(!mirrorControl.gameObject.activeInHierarchy);
+            mirrorControl.transform.position = transform.position + new Vector3(_playerFacing, 0, 0);
+        }
+    }
+}
